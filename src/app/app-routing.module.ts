3554c162c02aef7c7cb8bd846aca6d450bbff59b
brigremlin <brigrimsley@gmail.com>
2021-12-13 14:31:14 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './home/home.component';
 
-import { LoginComponent } from './login/login.component';
+
 import { MainComponent } from './main/main.component';
 import { SignupComponent } from './signup/signup.component';
 import { SuggestedFriendsComponent } from './suggested-friends/suggested-friends.component';
