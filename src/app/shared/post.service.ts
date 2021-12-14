6acc8e2ce74060c9501b6d7014a67b950189050b
Jordan Ludovissie <Jludovissie24@gmail.com>
2021-12-13 19:58:24 -0600

    Updated Home

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -12,10 +12,10 @@ import { Observable, BehaviorSubject  } from "rxjs";
 export class PostService{
 
   private post:  Post[] = [
-    {username: 'jollyjordan', post:'I am cool', likes: 0, dislikes: 0 },
-    {username: 'happyhannah', post:'I am happy', likes: 0 , dislikes: 0 },
-    {username:'niftynancy', post:'I am nifty', likes: 0, dislikes: 0  },
-    {username:'bestiebri' , post: 'I am your bestie', likes: 0 , dislikes: 0 }
+    {username: 'jollyjordan', post:'I am cool', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Onrus_Surfer.jpg/640px-Onrus_Surfer.jpg' , likes: 0, dislikes: 0 },
+    {username: 'happyhannah', post:'I am happy', image:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Egyptian_Surfer.jpg/640px-Egyptian_Surfer.jpg', likes: 0 , dislikes: 0 },
+    {username:'niftynancy', post:'I am nifty', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Pro_Surfers_Owen_Wright%2C_Gabriel_Medina_and_Miguel_Pupo_in_San_Francisco%2C_California_on_Nov._7th_2011._Photo_by_Ithaka_Darin_Pappas.jpg/640px-Pro_Surfers_Owen_Wright%2C_Gabriel_Medina_and_Miguel_Pupo_in_San_Francisco%2C_California_on_Nov._7th_2011._Photo_by_Ithaka_Darin_Pappas.jpg' , likes: 0, dislikes: 0  },
+    {username:'bestiebri' , post: 'I am your bestie', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Surfer_and_Northern_elephant_seal_at_Drakes_Beach.jpg/640px-Surfer_and_Northern_elephant_seal_at_Drakes_Beach.jpg', likes: 0 , dislikes: 0 }
   ];
 public post$: Observable<Post[]>
 private postSub: BehaviorSubject<Post[]> = new BehaviorSubject(this.post)
